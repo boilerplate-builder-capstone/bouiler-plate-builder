8863ba37d8450b5b2123b644fb79c024f5c93870
Ellie King <elliebking1@gmail.com>
2021-06-03 21:48:13 -0400

    removed bundle.js files

--- client/components/Counter.js
+++ client/components/Counter.js
@@ -5,13 +5,13 @@ function Counter() {
     
     const [count, setCount] = useState(0)
 
-    // TO HAVE COUNT PERSIST AFTER REFRESH: uncomment the commented code below (and on line 24)
-    // useEffect(() => {
-    //     if (count > 0){
-    //         window.localStorage.setItem("count", `${count}`)
-    //     }
-    //     setCount(JSON.parse(window.localStorage.getItem("count")))
-    // }, [count])
+    // useEffect + localStorage makes state persist after refresh
+    useEffect(() => {
+        if (count > 0){
+            window.localStorage.setItem("count", `${count}`)
+        }
+        setCount(JSON.parse(window.localStorage.getItem("count")))
+    }, [count])
 
     return (
         <div>
@@ -21,7 +21,7 @@ function Counter() {
             }}>Increment</button>
             <button onClick={() => {
                 setCount(0)
-                // window.localStorage.removeItem("count")
+                window.localStorage.removeItem("count")
             }}>Clear</button>
         </div>
     )
