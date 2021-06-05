302fee084041880a8e302f57f2f85b327a8c344b
Maciej Pie <maciejpiech18@gmail.com>
2021-06-05 11:02:51 -0500

    model passed

--- bin/sync/syncAndSeed.js
+++ bin/sync/syncAndSeed.js
@@ -6,6 +6,14 @@ const syncAndSeed = async () => {
   try {
     await db.sync({ force: true });
     // -------------import all code snippets here------------------
+    // delete below when we have an actual code snippet
+    await Code.create({
+      id: 'T1',
+      fileName: 'test.txt',
+      category: 'test',
+      title: 'test entry',
+      snippet: 'this is a test for testing purposes',
+    });
   } catch (error) {
     console.log('error occured in syncAndSeed', error);
   }
