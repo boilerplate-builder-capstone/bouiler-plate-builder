6171d827ee0a06b8d94fa6b5ea4765cf4e2209d0
Ellie King <elliebking1@gmail.com>
2021-06-06 09:59:45 -0400

    completed question logic

--- client/components/App.js
+++ client/components/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import NavBar from './NavBar';
-import JumboTron from './JumboTron'
-import Counter from './Counter'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './home/Home'
+import Build from './build/Build'
+import NavBar from './NavBar'
+import Questions from './Questions'
 
 
 function App() {
     
     return (
         <div>
-            <NavBar />
-            <JumboTron />
-            <Counter />
+            <Router>
+                <NavBar />
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route exact path="/build">
+                    <Build/>
+                </Route>
+                <Route exact path="/build/customize">
+                    <Questions />
+                </Route>
+            </Router>
         </div>
     )
 }
