83f97a560de1a2f87c5c219c3641b13063be6768
Maciej Pie <maciejpiech18@gmail.com>
2021-06-08 20:48:02 -0500

    added error handler for problem with the auth token

--- server/api/accessTokenRouter.js
+++ server/api/accessTokenRouter.js
@@ -4,12 +4,27 @@ const axios = require('axios');
 accessTokenRouter.get('/', async (req, res, next) => {
   try {
     let response = (
-      await axios.post('https://github.com/login/oauth/access_token', {
-        code: req.query.code,
-        client_id: process.env.client_id,
-        client_secret: process.env.client_secret,
-      })
+      await axios.post(
+        'https://github.com/login/oauth/access_token',
+        {
+          code: req.query.code,
+          client_id: process.env.client_id,
+          client_secret: process.env.client_secret,
+        },
+        {
+          // makes token error reading easier
+          headers: {
+            accept: 'application/json',
+          },
+        }
+      )
     ).data;
+    // error handler for not receiving a token
+    if (response.error) {
+      const error = Error(response.error);
+      error.status = 401;
+      throw error;
+    }
     res.send(response);
   } catch (error) {
     console.log('error in accessTokenRouter', error);
