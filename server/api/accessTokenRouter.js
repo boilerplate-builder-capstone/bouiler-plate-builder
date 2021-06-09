8b6b638bf4396d40cb65874543453840e4a1bf5c
Maciej Pie <maciejpiech18@gmail.com>
2021-06-08 19:26:33 -0500

    going to merge with master

--- server/api/accessTokenRouter.js
+++ server/api/accessTokenRouter.js
@@ -3,11 +3,13 @@ const axios = require('axios');
 
 accessTokenRouter.get('/', async (req, res, next) => {
   try {
-    let response = axios.post('https://github.com/login/oauth/access_token', {
-      code: req.query.code,
-      client_id: process.env.client_id,
-      client_secret: process.env.client_secret,
-    });
+    let response = (
+      await axios.post('https://github.com/login/oauth/access_token', {
+        code: req.query.code,
+        client_id: process.env.client_id,
+        client_secret: process.env.client_secret,
+      })
+    ).data;
     res.send(response);
   } catch (error) {
     console.log('error in accessTokenRouter', error);
