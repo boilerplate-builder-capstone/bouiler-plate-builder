93b40bfc170d2834ba3b8d7dbd06d4e671a792f4
Maciej Pie <maciejpiech18@gmail.com>
2021-06-09 20:32:07 -0500

    added new user model

---
+++ server/db/models/User.js
@@ -0,0 +1,20 @@
+const { DataTypes, UUIDV4 } = require('sequelize');
+const db = require('../db');
+
+const User = db.define('user', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    unique: true,
+  },
+  github: {
+    type: DataTypes.JSON,
+  },
+});
