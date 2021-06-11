efd3b6aba8fafbdf2b5742babbdc64ded9bfea57
Maciej Pie <76271066+mpiech1813@users.noreply.github.com>
2021-06-10 19:53:30 -0500

    Delete env.js

--- env.js
+++
@@ -1,7 +0,0 @@
-const JWT = 'superSneakySecret';
-process.env.JWT = JWT;
-
-module.exports = {
-  client_id: '9717988143b8c6ffcfb8',
-  client_secret: '192108873ed7eca1cbd8306977d3c58a0dbf1504',
-};
