cb2d9fedfadc55e402c2d45a1fdc410b4411b706
Maciej Pie <maciejpiech18@gmail.com>
2021-06-10 20:47:54 -0500

    updated User model

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,4 +1,7 @@
 const { DataTypes, UUIDV4 } = require('sequelize');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const db = require('../db');
 
 const User = db.define('user', {
@@ -6,6 +9,10 @@ const User = db.define('user', {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
+    defaultValue: 'Anonymous',
+  },
+  password: {
+    type: DataTypes.STRING,
   },
   id: {
     primaryKey: true,
@@ -18,5 +25,47 @@ const User = db.define('user', {
     type: DataTypes.JSON,
   },
 });
+
+// encrypts password after creation or change
+User.addHook('beforeSave', async (user) => {
+  if (user._changed.has('password')) {
+    user.password = await bcrypt.hash(user.password, 5);
+  }
+});
+
+// error handler
+const error = () => {
+  const err = Error('bad credentails, try again');
+  err.status = 401;
+  return err;
+};
+
+// takes username and password and return a token
+User.authenticate = async ({ username, password }) => {
+  const user = await User.findOne({
+    where: { username },
+  });
+  const password = await bcrypt.compare(password, user.password);
+  if (user && password) {
+    return await jwt.sign(user.id, process.env);
+  } else {
+    throw error();
+  }
+};
+
+// gets a user id from a token
+User.tokenId = async (token) => {
+  try {
+    const id = jwt.verify(token, process.env.JWT);
+    const user = await User.findByPk(id);
+    if (user) {
+      return user;
+    } else {
+      throw err();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = User;
