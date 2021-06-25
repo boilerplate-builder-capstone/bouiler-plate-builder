557a7032a6d9fd97c758976b00890b0c4d61b706
kfless12 <46584348+kfless12@users.noreply.github.com>
2021-06-24 20:34:29 -0400

    heroku deploy build

--- BOILERPLATE/server/db/db.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize')
-
-const dbName = 'capstonetest'
-const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost/${dbName}`)
-
-module.exports = db
